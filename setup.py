5d64f1bc387cc5671ccec2c659cb89b3c3da2e16
Tanmay98 <tanmaybaweja1999@gmail.com>
2023-12-28 14:04:11 -0700

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='multimodal_classification',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # already mentioned in requirements.txt
+    ],
+)
